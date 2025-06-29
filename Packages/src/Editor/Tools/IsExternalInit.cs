94674857089a347ccb979881c61bebf8073dad7e
hatayama <booqle@gmail.com>
2025-06-29 10:49:44 +0900

    recordのinit対応

---
+++ Packages/src/Editor/Tools/IsExternalInit.cs
@@ -0,0 +1,7 @@
+namespace System.Runtime.CompilerServices
+{
+    /// <summary>
+    /// Dummy type to support C# 9 init-only setter
+    /// </summary>
+    internal static class IsExternalInit { }
+}
